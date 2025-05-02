109f28ff63096f5700afac308b6c0c72b19aec1c
Матюшкина Мария <maria18112005@yandex.ru>
2025-05-02 22:28:41 +0700

    книги

---
+++ lab6/books.js
@@ -0,0 +1,101 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 750,
+        bestseller: false,
+        inStock: true
+
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 750,
+        bestseller: false,
+        inStock: true
+
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 750,
+        bestseller: true,
+        inStock: true
+
+    },
+    {
+        author: 'Роберт Кийосаки',
+        name: 'Богатый папа, бедный папа',
+        publicationYear: 2017,
+        price: 750,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Наполеон Хилл',
+        name: 'Думай и богатей',
+        publicationYear: 2004,
+        price: 600,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джордж Клейсон',
+        name: 'Самый богатый человек в Вавилоне',
+        publicationYear: 1926,
+        price: 550,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Бенджамин Грэм',
+        name: 'Разумный инвестор',
+        publicationYear: 1949,
+        price: 1100,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Тимоти Феррисс',
+        name: 'Как работать 4 часа в неделю',
+        publicationYear: 2007,
+        price: 900,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Дейл Карнеги',
+        name: 'Как завоёвывать друзей и оказывать влияние на людей',
+        publicationYear: 1936,
+        price: 700,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джон Богл',
+        name: 'Руководство разумного инвестора по взаимным фондам',
+        publicationYear: 1999,
+        price: 1000,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Виктор Франкл',
+        name: 'Сказать жизни «Да!»',
+        publicationYear: 1946,
+        price: 650,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Стивен Кови',
+        name: '7 навыков высокоэффективных людей',
+        publicationYear: 1989,
+        price: 850,
+        bestseller: true,
+        inStock: true
+    }
+];
